d769fa08015603e256f1cd11d2ff28caedd44ecd
Mohammad Fazle Karim <mohammadfaglekarim@gmail.com>
2020-01-31 20:12:58 +0600

    NewPracticesAdded

---
+++ VechicleAppPractice2/VechicleAppPractice2/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VechicleAppPractice2
+{
+     public class Vehicle
+    {
+        public string VehicleName { set; get; }
+        public string RegNo { set; get; }
+        
+        public List<double> speed = new List<double>();
+
+        public List<double> AddSpeed(double vehicleSpeed)
+        {
+            speed.Add(vehicleSpeed);
+            return speed;
+        }
+
+        public double MinSpeed()
+        {
+            double minSpeed = speed.Min();
+            return minSpeed;
+        }
+
+        public double MaxSpeed()
+        {
+            double maxSpeed = speed.Max();
+            return maxSpeed;
+        }
+
+        public double AverageSpeed()
+        {
+            double averageSpeed = speed.Average();
+            return averageSpeed;
+        }
+    }
+}
---
+++ VechicleAppPractice2/VechicleAppPractice2/VehicleUi.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VechicleAppPractice2
+{
+    public partial class VehicleUi : Form
+    {
+        public int v;
+
+        public VehicleUi()
+        {
+            v = 0;
+            InitializeComponent();
+        }
+
+        Vehicle vehicle = new Vehicle();
+        List<double> speed = new List<double>();
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(vehicleNameTextBox.Text))
+            {
+                MessageBox.Show("Vehicle name cannot be empty!");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(regNoTextBox.Text))
+            {
+                MessageBox.Show("Reg No cannot be empty!");
+                return;
+            }
+
+            v++;
+            vehicle.VehicleName = vehicleNameTextBox.Text;
+            vehicle.RegNo = regNoTextBox.Text;
+
+            vehicleNameTextBox.Clear();
+            regNoTextBox.Clear();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if(v >= 1)
+            {
+                if (String.IsNullOrEmpty(speedTextBox.Text))
+                {
+                    MessageBox.Show("Speed cannot be empty!");
+                    return;
+                }
+
+                vehicle.AddSpeed(Convert.ToDouble(speedTextBox.Text));
+
+                speedTextBox.Clear();
+            }
+            else
+                MessageBox.Show("Create a vehicle First");
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            if (v >= 1)
+            {
+                minSpeedTextBox.Text = vehicle.MinSpeed().ToString();
+                maxSpeedTextBox.Text = vehicle.MaxSpeed().ToString();
+                averageSpeedTextBox.Text = vehicle.AverageSpeed().ToString();
+                minSpeedTextBox.ReadOnly = true;
+                maxSpeedTextBox.ReadOnly = true;
+                averageSpeedTextBox.ReadOnly = true;
+            }
+            else
+                MessageBox.Show("Create a Vehicle First");
+        }
+    }
+}
